2454df90beff7155166b3f23912c6595ef779fa8
chrischeng <357213557@qq.com>
2016-06-13 11:49:44 +0800

    override setPressed() and update version to 1.0.1

--- selectorview/src/main/java/com/chrischeng/selectorview/SelectorImageView.java
+++ selectorview/src/main/java/com/chrischeng/selectorview/SelectorImageView.java
@@ -5,16 +5,11 @@ import android.content.res.TypedArray;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
-import android.view.ViewConfiguration;
 import android.widget.ImageView;
 
 public class SelectorImageView extends ImageView {
 
     private int mFilterColor;
-    private boolean mIsOutside;
-    private boolean mIsLongClick;
-    private LongPressedRunnable mLongPressedRunnable;
 
     public SelectorImageView(Context context) {
         this(context, null);
@@ -22,7 +17,6 @@ public class SelectorImageView extends ImageView {
 
     public SelectorImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mLongPressedRunnable = new LongPressedRunnable();
         initAttrs(attrs);
     }
 
@@ -31,70 +25,21 @@ public class SelectorImageView extends ImageView {
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getActionMasked()) {
-            case MotionEvent.ACTION_DOWN:
-                setFilter();
-                if (isLongClickable())
-                    postDelayed(mLongPressedRunnable, ViewConfiguration.getLongPressTimeout());
-                break;
-            case MotionEvent.ACTION_MOVE:
-                checkInBound(event.getX(), event.getY());
-                break;
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_CANCEL:
-                performClickEvent();
-                resetStatus();
-                mIsOutside = false;
-                mIsLongClick = false;
-                break;
-        }
+    public void setPressed(boolean pressed) {
+        super.setPressed(pressed);
 
-        return true;
+        Drawable drawable = getDrawable() == null ? getBackground() : getDrawable();
+        if (drawable != null) {
+            if (pressed)
+                drawable.setColorFilter(mFilterColor, PorterDuff.Mode.MULTIPLY);
+            else
+                drawable.clearColorFilter();
+        }
     }
 
     private void initAttrs(AttributeSet attrs) {
         TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.SelectorImageView);
         mFilterColor = a.getColor(R.styleable.SelectorImageView_siv_filter_color, getResources().getColor(R.color.default_filter));
         a.recycle();
-    }
-
-    private void setFilter() {
-        Drawable drawable = getDrawable() == null ? getBackground() : getDrawable();
-        if (drawable != null)
-            drawable.setColorFilter(mFilterColor, PorterDuff.Mode.MULTIPLY);
-    }
-
-    private void clearFilter() {
-        Drawable drawable = getDrawable() == null ? getBackground() : getDrawable();
-        if (drawable != null)
-            drawable.clearColorFilter();
-    }
-
-    private void checkInBound(float x, float y) {
-        if (x < 0 || x > getWidth() || y < 0 || y > getHeight()) {
-            mIsOutside = true;
-            resetStatus();
-        }
-    }
-
-    private void resetStatus() {
-        clearFilter();
-        removeCallbacks(mLongPressedRunnable);
-    }
-
-    private void performClickEvent() {
-        if (isClickable() && !mIsOutside && !mIsLongClick)
-            performClick();
-    }
-
-    private class LongPressedRunnable implements Runnable {
-
-        @Override
-        public void run() {
-            mIsLongClick = true;
-            performLongClick();
-            resetStatus();
-        }
     }
 }
